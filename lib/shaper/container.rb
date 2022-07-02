bd7feab7f06ab9b0c78e8017d7ffcbb783f95bc3
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-07-02 18:21:27 +0200

    Simple implementation

--- lib/shaper/container.rb
+++ lib/shaper/container.rb
@@ -2,10 +2,21 @@
 
 module Shaper
   class Container
+    class UnknownEventError < Shaper::Error; end
+
     def initialize(reducer)
+      @reducer = reducer
       @state = reducer.initial_state
     end
 
     def state = Marshal.load(Marshal.dump(@state))
+
+    def dispatch(event_name, payload)
+      raise UnknownEventError, "Handler for '#{event_name}' is not found" unless @reducer.respond_to?(event_name)
+
+      @state = @reducer.public_send(event_name, state, payload)
+      # TODO: notify subscribers
+      nil
+    end
   end
 end
--- spec/shaper/container_spec.rb
+++ spec/shaper/container_spec.rb
@@ -6,7 +6,15 @@ RSpec.describe Shaper::Container do
       include Shaper::Reducer
 
       def initial_state
-        {}
+        0
+      end
+
+      def increase(state, payload)
+        state + payload
+      end
+
+      def decrease(state, payload)
+        state - payload
       end
     end
   end
@@ -16,7 +24,32 @@ RSpec.describe Shaper::Container do
     subject { container.state }
 
     it "returns current state" do
-      expect(subject).to eq({})
+      expect(subject).to eq(0)
+    end
+  end
+
+  describe "#dispatch" do
+    subject { container.dispatch(event_name, payload) }
+
+    let(:payload) { 1 }
+
+    context "when event handler exists" do
+      it "modifies state" do # rubocop:disable RSpec/MultipleExpectations
+        container.dispatch(:increase, 1)
+        expect(container.state).to eq(1)
+        container.dispatch(:decrease, 1)
+        expect(container.state).to eq(0)
+      end
+    end
+
+    context "when event handler does not exist" do
+      let(:event_name) { "unknown" }
+
+      it "raises an error" do
+        expect do
+          subject
+        end.to raise_error(described_class::UnknownEventError, "Handler for 'unknown' is not found")
+      end
     end
   end
 end
